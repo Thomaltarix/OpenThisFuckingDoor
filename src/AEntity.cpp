e84168947857f538cc638c1bc3e506ca40753085
eliotttesnier <eliott.tesnier@epitech.eu>
2024-03-22 22:00:31 +0100

    [ADD] AEntitiy .hpp && .cpp

---
+++ src/AEntity.cpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2024
+** OpenThisFuckingDoor
+** File description:
+** AEntitiy
+*/
+
+#include "AEntity.hpp"
+AEntity::AEntity()
+{
+}
+
+AEntity::AEntity()
+{
+}
+
+void AEntity::setTextures(std::vector<std::string> strings)
+{
+    _textures.clear();
+    for (auto& path : strings) {
+        sf::Texture texture;
+        if (!texture.loadFromFile(path))
+            throw Error("Failed to load texture.");
+        _textures.push_back(texture);
+    }
+}
