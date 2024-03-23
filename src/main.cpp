ca488e2b749232188b5879ecee4d93d0fdf084d2
Nolan Papa <nolan.papa@epitech.eu>
2024-03-23 23:23:39 +0100

    UPDATE remove getgame function

--- src/main.cpp
+++ src/main.cpp
@@ -5,30 +5,24 @@
 ** main
 */
 
+#include "GameMenu/GameMenu.hpp"
 #include "Game.hpp"
-#include "Menu.hpp"
 #include "button.hpp"
 
-Game *getGame()
-{
-    static Game game;
-    return &game;
-}
+Game game;
 
 int main(int argc, char **argv)
 {
-    Game *game = getGame();
-    Menu menu;
     int event;
     (void) argc;
     (void) argv;
 
-    game->playMusic();
-    while (game->isWindowOpen()) {
-        event = game->getKeyEvent();
-        game->clearWindow();
-        menu.displayMenu();
-        game->DisplayWindow();
+    game.playMusic();
+    while (game.isWindowOpen()) {
+        event = game.getKeyEvent();
+        game.clearWindow();
+        game.getGameMenu()->displayGameMenu();
+        game.DisplayWindow();
     }
     (void) event;
     return 0;
