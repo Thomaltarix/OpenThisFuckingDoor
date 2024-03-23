fad11ded0ac63f5d0948df765ebaaa49db93b658
eliotttesnier <eliott.tesnier@epitech.eu>
2024-03-23 10:40:41 +0100

    class pnj -> anchor point, dialogs

---
+++ src/PNJ.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2024
+** OpenThisFuckingDoor
+** File description:
+** Player
+*/
+
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <vector>
+#include <unordered_map>
+#include <AEntity.hpp>
+#include <Move.hpp>
+
+class PNJ : public Move {
+    public:
+        PNJ() = default;
+        ~PNJ() = default;
+
+        // Setters
+        void setAnchor(float x, float y) {_anchor = std::pair<float, float>(x, y);}
+        void setDialgos(std::vector<std::string> dialogs) {_dialogs = dialogs;}
+
+        // Getters
+        std::pair<float, float> getAnchor(void) {return _anchor;}
+        std::vector<std::string> getDialogs(void) {return _dialogs;}
+    protected:
+        std::pair<float, float> _anchor;
+        std::vector<std::string> _dialogs;
+    private:
+};
