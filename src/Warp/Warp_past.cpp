7704e23a0276e691cb833e4a3c320ccac80529f2
nathan bezard <nathan.bezard@epitech.eu>
2024-03-24 20:51:11 +0100

    Added all the warp

---
+++ src/Warp/Warp_past.cpp
@@ -0,0 +1,13 @@
+/*
+** EPITECH PROJECT, 2024
+** OpenThisFuckingDoor
+** File description:
+** Warp_past
+*/
+
+#include "Warp_past.hpp"
+
+Warp_past::Warp_past(std::string str, std::pair<int, int> pos, std::pair<int, int> size)
+{
+    data["Timeline"] = GameMap::PAST;
+}
