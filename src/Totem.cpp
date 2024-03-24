f21721a31302aa08a32b589bd230ffc46efdf5a0
ChifuyuOnWish <noah.auroy@epitech.eu>
2024-03-24 16:35:39 +0100

    ADDED class totem

---
+++ src/Totem.cpp
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2024
+** OpenThisFuckingDoor
+** File description:
+** Totem
+*/
+
+#include <HitboxModule.hpp>
+#include <MovementModule.hpp>
+#include "Totem.hpp"
+#include "Game.h"
+#include <Game.hpp>
+#include "GameObject.hpp"
+
+void totem_action(GameObject *player, GameObject *totem)
+{
+    player->data["hasTotem"] = true;
+    totem->data["x"] = -1000;
+    totem->data["y"] = -1000;
+}
+
+Totem::Totem(std::string str, std::pair<int, int> pos, std::pair<int, int> size)
+{
+    addModule<InteractionModule>();
+    addModule<DisplayModule>();
+    data["texture"] = str;
+    data["x"] = pos.first;
+    data["y"] = pos.second;
+    data["TextureSize"] = size;
+    data["actionEffect"] = &totem_action;
+}
